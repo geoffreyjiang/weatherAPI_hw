ee0629dc2edc7955de6b4246b9b41c1964f6dff8
Geoffrey Jiang <geoffreychjiang@gmail.com>
2020-11-04 22:04:21 -0800

    btn function

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,38 @@
 let apiKey = "10a0190a15115135e5711a7b089580be";
+
+
+function searchFunction(searchInput) {
+    $.ajax
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+    $(searchBtn).on("click", function(){
+        var searchVal = $(".searchInput").val();
+
+
+
+
+    })
+
+
+
+})
