28612e06c0a0097dc1ec68da2d4ec7c2b6437692
Geoffrey Jiang <geoffreychjiang@gmail.com>
2020-11-05 00:56:24 -0800

    more css and js

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,14 @@
-let apiKey = "10a0190a15115135e5711a7b089580be";
-// "api.openweathermap.org/data/2.5/forecast?q=" + searchVal  + "&appid=10a0190a15115135e5711a7b089580be" city info
-// "https://api.openweathermap.org/data/2.5/uvi?appid=10a0190a15115135e5711a7b089580be&lat=" + lat + "&lon=" + lon, weather icons
-
+var apiKey = "10a0190a15115135e5711a7b089580be";
+var searchBtn = $(".searchBtn");
+var searchInput = $(".searchInput");
+var cityNameEl = $(".cityName");
+var currentDateEl = $(".currentDate");
+var weatherIconEl = $(".weatherIcon");
+var searchHistoryEl = $(".searchHistory");
+var tempEl = $(".temp");
+var humidityEl = $(".humidity");
+var windSpeedEl = $(".speed");
+var uvIndexEl = $(".uvIndex");
 
 
 
