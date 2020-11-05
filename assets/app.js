deaec22b384e347c1dc4ffc639d0a315275c4edd
Geoffrey Jiang <geoffreychjiang@gmail.com>
2020-11-04 22:29:54 -0800

    history js

--- assets/app.js
+++ assets/app.js
@@ -1,38 +1,37 @@
 let apiKey = "10a0190a15115135e5711a7b089580be";
 
 
-function searchFunction(searchInput) {
-    $.ajax
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
 
 $(document).ready(function(){
-    $(searchBtn).on("click", function(){
+    $(".searchBtn").on("click", function(){
         var searchVal = $(".searchInput").val();
-
-
-
-
+        $(".searchInput").val("");
+        cityFunction(searchVal)
     })
 
+    var cityHistory = JSON.parse(localStorage.getItem("history"));
+
+    if (cityHistory.length > 0) {
+        cityFunction(history[history.length - 1]);
+    }
+    for (var i = 0; i < cityHistory.length; i++) {
+        makeRow(searchHistory[i])
+    }
+
+    function makeRow(text){
+        var cityList = $("<li>").addClass("list-group-item").text();
+        $(".searchHistory").append(cityList);
+    }
+
+    $(".searchHistory").on("click", "li", function (){
+        cityFunction($(this).text())
+    })
+
+
+    
 
 
 })
