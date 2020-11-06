d001244becb1468f13392c162470e2ccd69199a7
Geoffrey Jiang <geoffreychjiang@gmail.com>
2020-11-05 17:26:51 -0800

    added json and function

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,30 @@
 var apiKey = "10a0190a15115135e5711a7b089580be";
 var searchBtn = $(".searchBtn");
 var searchInput = $(".searchInput");
-var cityNameEl = $(".cityName");
-var currentDateEl = $(".currentDate");
-var weatherIconEl = $(".weatherIcon");
-var searchHistoryEl = $(".searchHistory");
-var tempEl = $(".temp");
+var cityName = $(".cityName");
+var currentDate = $(".currentDate");
+var weatherIcon = $(".weatherIcon");
+var searchHistory = $(".searchHistory");
+var temp = $(".temp");
 var humidityEl = $(".humidity");
 var windSpeedEl = $(".speed");
 var uvIndexEl = $(".uvIndex");
 
 
 
+if(JSON.parse(localStorage.getItem("searchHistory")) === null) {
+        console.log("Search History not found")
+}else  {
+        console.log("Search History found");
+
+    }
+
+function cityInfo(cityName){
+    searchHistory.empty();
+    var searchArr = JSON.parse(localStorage.getItem("searchHistory"));
+    for (var i = 0; i < searchArr.length; i++) {
+        let listItem = $("<li>").attr("class", newEntry);
+        listItem.text(searchArr[i])
+        listItem.append.searchHistory
+    }
+}
